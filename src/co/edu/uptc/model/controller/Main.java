aa9603b923a0d478e7e51ef527b25d1999685a54
Sebas-Arias-01 <sebastian.arias02@uptc.edu.co>
2025-05-21 08:43:48 -0500

    creación main

---
+++ src/co/edu/uptc/model/controller/Main.java
@@ -0,0 +1,14 @@
+package co.edu.uptc.model.controller;
+
+import java.util.List;
+
+import co.edu.uptc.model.Order;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+    }
+
+}
